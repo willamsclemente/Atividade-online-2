64b30cfe641870dbd1c2c3783d5fa1ba7de4033b
willamsclemente <wila_clemente@hotmail.com>
2022-09-03 17:24:20 -0300

    Atividade 2 realizada

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 0,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        435:{
+            items:2
+        },
+        992:{
+            items:3
+        },
+
+        1200:{
+            items:3
+        }
+    }
+});
